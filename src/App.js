7d78196414ee2a945df17bc7a38df69988b7dbac
Saurabh <saurabh100rab002@gmail.com>
2023-09-17 11:15:34 +0000

    final

--- src/App.js
+++ src/App.js
@@ -5,3 +5,7 @@ import './App.css'
 const App = () => <ClickCounter />
 
 export default App
+
+//This is the mail folder all the component will come and
+//add here within App function , it will compile the js code
+// also it will useful to add comments.
